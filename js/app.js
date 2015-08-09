51558bdc652fb69aa3610ee9f042be223bc345fe
Scott Cytacki <scytacki@concord.org>
2015-08-09 12:57:37 -0400

    more helpful comment

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var React = require('react');
 var App = require('./views/app');
 
 // use of the ref with a call back is a hack to keep things simple
+// it exposes the toplevel coponent as a global so other components can
+// send it events
 React.render(
   <App ref={function(component){ window.WS = component}}/>,
   document.getElementById('example')
