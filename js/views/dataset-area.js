7e89ef6c52a4a04207aa1ca6ed61bcad14304c9e
Piotr Janik <janikpiotrek@gmail.com>
2016-11-04 12:41:26 +0100

    Add new visualization tabs
    [#133600105]

--- js/views/dataset-area.js
+++ js/views/dataset-area.js
@@ -1,18 +1,67 @@
 var React = require('react');
 var TabbedArea = require('react-bootstrap/lib/tabbedarea');
 var TabPane = require('react-bootstrap/lib/tabpane');
-var DatasetList = require('./dataset-list')
+var DatasetList = require('./dataset-list');
 var DatasetVisualization = require('./dataset-visualization');
 var ProjectDataEntry = require('./project-data-entry');
+
+const VIS_TABS = {
+  team: {
+    name: 'Visualize Team',
+    eventKey: 'visualizeTeam'
+  },
+  class: {
+    name: 'Visualize Class',
+    eventKey: 'visualizeClass'
+  },
+  teacher: {
+    name: 'Visualize Teacher',
+    eventKey: 'visualizeTeacher'
+  },
+  state: {
+    name: 'Visualize State',
+    eventKey: 'visualizeState'
+  },
+  all: {
+    name: 'Visualize All States',
+    eventKey: 'visualizeAllStates'
+  }
+};
 
 var DatasetArea = React.createClass({
   handleTabSelect: function (key) {
     this.props.onSelect(key);
   },
 
+  getDatasetList: function (type) {
+    if (this.props.filteredDatasets === 'refreshing') {
+      // Special case, datasets are refreshing. DatasetVisualization handles this value too.
+      return 'refreshing';
+    }
+    if (type === 'all') {
+      // Another special case. Filtering doesn't provid all the datasets, as it's not necessary.
+      // Also, we can't list all the IDs, as URL would get too long. If we don't provide anything, the effect is the same.
+      return '';
+    }
+    return this.props.filteredDatasets && this.props.filteredDatasets[type];
+  },
+
+  renderVisualizeTab: function (type) {
+    var tabInfo = VIS_TABS[type];
+    return (
+      <TabPane key={type} eventKey={tabInfo.eventKey} tab={tabInfo.name}>
+        <DatasetVisualization
+          datasetList={this.getDatasetList(type)}
+          project={this.props.project}
+          classPeriod={this.props.classPeriod}
+          team={this.props.team}/>
+      </TabPane>
+    );
+  },
+
   render: function() {
     var addDatasetTab =
-      <TabPane eventKey='add' tab='Add Dataset'>
+      <TabPane key='add' eventKey='add' tab='Add Dataset'>
         <ProjectDataEntry
           project={this.props.project}
           classPeriod={this.props.classPeriod}
@@ -20,26 +69,18 @@ var DatasetArea = React.createClass({
           progress={this.props.submissionProgress}/>
       </TabPane>;
     var listDatasetsTab =
-      <TabPane eventKey='list' tab='List Datasets'>
+      <TabPane key='list' eventKey='list' tab='List Datasets'>
         <DatasetList
           project={this.props.project}
           datasets={this.props.datasets}
           classPeriod={this.props.classPeriod}/>
       </TabPane>;
-    var visualizeDatasetsTab =
-      <TabPane eventKey='visualize' tab='Visualize Datasets'>
-        <DatasetVisualization
-          project={this.props.project}
-          classPeriod={this.props.classPeriod}
-          team={this.props.team}
-          teamDatasetList={this.props.teamDatasetList}
-          visible={this.props.tabKey === 'visualize'}/>
-      </TabPane>;
 
     var tabs = [addDatasetTab, listDatasetsTab];
     // if we are embedded then add a new tab for visualize
-    if(window.EMBEDDED){
-      tabs = [addDatasetTab, visualizeDatasetsTab];
+    if (window.EMBEDDED) {
+      var visTabs = Object.keys(VIS_TABS).map(function (type) { return this.renderVisualizeTab(type); }.bind(this));
+      tabs = [addDatasetTab].concat(visTabs);
     }
 
     return (
--- js/views/dataset-visualization.js
+++ js/views/dataset-visualization.js
@@ -1,8 +1,24 @@
 var React = require('react');
 
+// It seems iSENSE returns 403 error if URL is longer than this.
+const MAX_URL_LENGTH = 4090;
+
+function getURL(server, projectId, datasetList) {
+  return server + "/projects/" + projectId + "/data_sets/" + datasetList + "?embed=true";
+}
+
+function truncateURL(url) {
+  while (url.length > MAX_URL_LENGTH) {
+    // Remove first dataset ID.
+    url = url.replace(/\/\d*,/, '/');
+  }
+  return url;
+}
+
 var DatasetVisualization = React.createClass({
-  render: function() {
-    if(this.props.project === null ||
+
+  render: function () {
+    if (this.props.project === null ||
       this.props.classPeriod === null ||
       this.props.team === null){
       return (
@@ -12,25 +28,23 @@ var DatasetVisualization = React.createClass({
       );
     }
 
-    if (this.props.teamDatasetList === null){
+    if (this.props.datasetList === null) {
+      return <div> There is no data.</div>;
+    } else if (this.props.datasetList === 'refreshing') {
+      return <div>Visualization is updating...</div>;
+    } else {
+      var url = getURL(this.props.project.server, this.props.project.id, this.props.datasetList);
+      var truncated = false;
+      if (url.length > MAX_URL_LENGTH) {
+        url = truncateURL(url);
+        truncated = true;
+      }
       return (
         <div>
-          There is no data for this team.
-        </div>);
-    } else if (this.props.teamDatasetList === 'refreshing'){
-      return (
-        <div>Visualization is updating...</div>
+          {truncated && <div><b>Warning:</b> can't display all the datasets, some data was truncated.</div>}
+          <iframe src={url} width='100%' height='400' allowFullScreen='true'></iframe>
+        </div>
       );
-    } else {
-      if(this.props.visible){
-        var url = this.props.project.server + "/projects/" + this.props.project.id +
-                  "/data_sets/" + this.props.teamDatasetList + "?embed=true";
-        return (
-          <iframe src={ url } width='100%' height='400' allowFullScreen='true'></iframe>
-          );
-      } else {
-        return (<div>Visualization should not be visible</div>)
-      }
     }
   }
 });
